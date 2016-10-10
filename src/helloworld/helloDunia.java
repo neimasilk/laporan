e171e2d072aa895133b3ba8ba4058a73b3b4e302
neimasilk <neimasilk@yahoo.com>
2016-10-10 15:38:18 +0700

    percobaan1

---
+++ src/helloworld/helloDunia.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class helloDunia {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Halo Dunia");
+
+	}
+
+}
