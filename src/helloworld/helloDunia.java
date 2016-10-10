f77a555ebe8094f9cf0c5d5af27a5b542735b71c
neimasilk <neimasilk@yahoo.com>
2016-10-10 15:42:43 +0700

    penambahan halo dunia 2

--- src/helloworld/helloDunia.java
+++ src/helloworld/helloDunia.java
@@ -8,6 +8,7 @@ public class helloDunia {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Halo Dunia");
+		System.out.print("halo dunia 2");
 
 	}
 
